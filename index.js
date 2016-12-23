c8a0f0c183f144ff31f3facf43bd8c134c7a0b7a
mvinuesa <mvinuesa@servexternos.isban.es>
2016-12-23 14:39:02 +0100

    DEVSTCK-1706 add comments and node best practice

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/**
+* @fileOverview Midleware propagation and validation JWT token.
+
+* @author Noel Rodriguez
+* @version 1.0.0
+
+* @example
+* var koa = require('koa');
+* var koaPropagation = require(koa-devstack-propagation);
+* var app = new koa();
+* app.use(koaPropagation());
+*/
 'use strict'
 
 var jwt        = require('jsonwebtoken');
@@ -5,31 +17,33 @@ var appRoot    = require('app-root-path');
 var config     = require(appRoot + '/configuration.json');
 var rp = require('request-promise');
 
-/**
-* Midleware validation token
-*
-*/
-
 var token    =  '';
+
+/**
+* Validate and propagate JWT token
+* @returns {next} next function from koa middelware
+*/
 module.exports = function() {
   const bearer =  'bearer';
-  return function checktoken (ctx, next) {
-    if (ctx == null || ctx === 0 || Object.keys(ctx).length === 0){
+  return function checktoken(ctx, next) {
+    var isContext = (ctx == null || ctx === 0 || Object.keys(ctx).length === 0);
+    if (isContext) {
       return ctx.throw(401, 'Bad Authorization header format. Format is "Authorization: Bearer <token>"\n');
-    }else {
+    } else {
       const authorization  = ctx.request.headers.authorization;
       if (authorization) {
         var parts = authorization.split(' ');
-        if (parts.length === 2 && parts[0] === bearer) {
+        var isAuthorizationBearer = (parts.length === 2 && parts[0] === bearer);
+        if (isAuthorizationBearer) {
           token = parts[1];
           ctx.state.authorizationHeader = authorization;
           validateToken();
           return next();
-        }else{
+        } else {
           return ctx.throw(401, 'Bad Authorization header format. Format is "Authorization: Bearer <token>"\n');
         }
-      }else{
-        return ctx.throw(401,"JWT token is mandatory");
+      } else {
+        return ctx.throw(401, 'JWT token is mandatory');
       }
     }
   }
@@ -37,60 +51,59 @@ module.exports = function() {
 
 /**
 * This function is in charge on get the public key and validate it.
-*
-*
 */
-async function   validateToken() {
-      var serverPublicKey = config.publicKeyProvider.url + '/' + config.publicKeyProvider.keyIdentifier;
-      console.log("serverPublicKey",serverPublicKey);
-      var publicKey="";
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-      var options = {
-        url: serverPublicKey,
-        headers: { 'User-Agent': 'request' }
-      };
-      rp(serverPublicKey)
-        .then(function (htmlString) {
-          var info = JSON.parse(htmlString);
-          publicKey = info.key;
-          verifyToken(publicKey);
-        })
-        .catch(function (err) {
-            console.log("Error",err);
-        });
-      console.log("token es",publicKey);
+async function validateToken() {
+  var serverPublicKey = config.publicKeyProvider.url + '/' + config.publicKeyProvider.keyIdentifier;
+  console.log('serverPublicKey',serverPublicKey);
+  var publicKey='';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  var options = {
+    url: serverPublicKey,
+    headers: { 'User-Agent': 'request' }
+  };
+  rp(serverPublicKey)
+    .then(function (htmlString) {
+      var info = JSON.parse(htmlString);
+      publicKey = info.key;
+      verifyToken(publicKey);
+    })
+    .catch(function (err) {
+        console.log('Error',err);
+    });
+  console.log('token es',publicKey);
 }
 
 /**
 * This function validate the jwt token
 *
 * @param  {string} publicKey The public key.
-* @throw  error 401 if the token has expired.
+* @returns error 401 if the token has expired.
 */
 
-function verifyToken(publicKey){
-      try{
-        var decoded = jwt.verify(token, base64toPem(publicKey), { algorithms: ['RS256'] });
-      }catch(err){
-        return ctx.throw(401,"The JWT token is invalid");
-      }
-      if (decoded.exp < Date.now() / 1000) {
-          return ctx.throw(401,"This JWT has expired");
-      }
+function verifyToken(publicKey) {
+  try {
+    var decoded = jwt.verify(token, base64toPem(publicKey), { algorithms: ['RS256'] });
+  } catch(err) {
+    return ctx.throw(401, 'The JWT token is invalid');
+  }
+  var isExpired = decoded.exp < Date.now() / 1000;
+  if (isExpired) {
+      return ctx.throw(401, 'This JWT has expired');
+  }
 }
 
 /**
 * This function convert the key to a pem file.
 *
 * @param  {string} input The text to convert a pem file.
-* @return {String} The resolved token
+* @return {String} public key in pem pormat (begin public key + doby + end public key)
 */
-function base64toPem(input){
+function base64toPem(input) {
   console.log(input);
-  var begin = "-----BEGIN PUBLIC KEY-----\n";
-  var end   = "-----END PUBLIC KEY-----"
-  for(var result="", lines=0;result.length-lines < input.length;lines++) {
-          result+=input.substr(result.length-lines,64)+"\n"
-      }
+  var begin = '-----BEGIN PUBLIC KEY-----\n';
+  var end   = '-----END PUBLIC KEY-----';
+  for(var result='', lines=0;result.length-lines < input.length;lines++) {
+    result+= input.substr(result.length-lines,64) + '\n';
+  }
   return begin + result + end;
 }
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const koajwt = require('./index')
 const request = require('supertest')
 var koa     = require('koa');
 describe('Test without authorization header', function () {
-  it('should throw 401 if no authorization header', function(done) {
+    it('should throw 401 if no authorization header', function(done) {
       var app = new koa();
       app.use(koajwt({ secret: 'shhhh' }));
       request(app.listen())
@@ -20,24 +20,23 @@ describe('Test without authorization header', function () {
           .set('Authorization', 'bearer 1234567890')
           .expect(404)
           .end(done);
-      });
-      it('should throw 401 with invalid token', function(done) {
-          var app = new koa();
-          app.use(koajwt({ secret: 'shhhh' }));
-          request(app.listen())
-            .get('/')
-            .set('Bla', 'bla1234567890')
-            .expect(401)
-            .end(done);
-        });
-
-        it('should throw 404 with token JWT',function(done){
-          var app = new koa();
-          app.use(koajwt({ secret: 'shhhh' }));
-          request(app.listen())
-            .get('/')
-            .set('Authorization', 'bearer eyJraWQiOiJJRFBTRVJFTklUWV9TSEExd2l0aFJTQSIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJ4MDIxMDk2IiwiYXVkIjoiU0FSQVNFUkVOSVRZIiwibmJmIjoxNDczNzU5ODk0LCJpc3MiOiJJRFBTRVJFTklUWSIsImV4cCI6MzYwMTQ3Mzc1OTg5NCwiaWF0IjoxNDczNzU5ODk0LCJqdGkiOiI5MDgxMTk0NS1jMjIyLTRkOWQtYWY5NS04MjAwOTRiMDg3ZmYifQ.a5hNefm9KuEfp-cjt9_-p6tSeMEVMvHVHAry175eV_dOVloPB0gKnlh4x7GbTc4egpstX0JNZzMN5pojLHj4efW-1fREIWpzyGuup4KXoqLXcJxihx22x1-Fmru2dkeY2tPVIwI99sCIn_RdwyPhDALK6WsRO3OOe8_MRSitTYfObaQLLQ-QMyrh8Yygc9FQvcxKOgH7S8dwrU4IcnUOxJAW7tCq_0xZtBd_HRfEzhCIytIfcSxw1gvRNbuxgx5ZDgwOvqgJCS4MyK6ye1Di6ZITvNq5oYNJ5aA5qyohqJ_M-Q1INaBnVO5qUIotjG0m_9WH1OcCVJr_U_86aCoDCg')
-            .expect(404)
-            .end(done);
-        });
+    });
+    it('should throw 401 with invalid token', function(done) {
+      var app = new koa();
+      app.use(koajwt({ secret: 'shhhh' }));
+      request(app.listen())
+        .get('/')
+        .set('Bla', 'bla1234567890')
+        .expect(401)
+        .end(done);
+    });
+    it('should throw 404 with token JWT',function(done){
+      var app = new koa();
+      app.use(koajwt({ secret: 'shhhh' }));
+      request(app.listen())
+        .get('/')
+        .set('Authorization', 'bearer eyJraWQiOiJJRFBTRVJFTklUWV9TSEExd2l0aFJTQSIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJ4MDIxMDk2IiwiYXVkIjoiU0FSQVNFUkVOSVRZIiwibmJmIjoxNDczNzU5ODk0LCJpc3MiOiJJRFBTRVJFTklUWSIsImV4cCI6MzYwMTQ3Mzc1OTg5NCwiaWF0IjoxNDczNzU5ODk0LCJqdGkiOiI5MDgxMTk0NS1jMjIyLTRkOWQtYWY5NS04MjAwOTRiMDg3ZmYifQ.a5hNefm9KuEfp-cjt9_-p6tSeMEVMvHVHAry175eV_dOVloPB0gKnlh4x7GbTc4egpstX0JNZzMN5pojLHj4efW-1fREIWpzyGuup4KXoqLXcJxihx22x1-Fmru2dkeY2tPVIwI99sCIn_RdwyPhDALK6WsRO3OOe8_MRSitTYfObaQLLQ-QMyrh8Yygc9FQvcxKOgH7S8dwrU4IcnUOxJAW7tCq_0xZtBd_HRfEzhCIytIfcSxw1gvRNbuxgx5ZDgwOvqgJCS4MyK6ye1Di6ZITvNq5oYNJ5aA5qyohqJ_M-Q1INaBnVO5qUIotjG0m_9WH1OcCVJr_U_86aCoDCg')
+        .expect(404)
+        .end(done);
+    });
 })
